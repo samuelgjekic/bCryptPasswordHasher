68b958db1bbe6e54ee8237f55161fce1aa8bf3f6
salko gjekic <sami42411@gmail.com>
2023-09-10 20:59:38 +0200

    Add project files.

---
+++ TestEncryption/Encryption.cs
@@ -0,0 +1,42 @@
+﻿using BCrypt;
+using BCrypt.Net;
+
+namespace PasswordHasher
+{
+    public class Encryption
+    {
+        public Encryption()
+        { // Constructor
+        }
+
+        public string EncryptPassword(string _password)
+        {
+            try
+            {
+                string _salt = BCrypt.Net.BCrypt.GenerateSalt(); // Generate Salt
+                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(_password, _salt); // Encrypt the password using BCrypt hasher.
+                return hashedPassword; // Return the hash
+            } catch (Exception ex) 
+            {
+                return "Error with Encryption!";
+                throw new Exception(ex.Message);
+            }
+            
+        }
+        
+        public bool VerifyPassword(string _password, string _hash)  // Recieve a password and a hash
+        {
+            try
+            {
+                bool validPassword = BCrypt.Net.BCrypt.Verify(_password, _hash); // Check the user input with hashcode from DB Table
+                return validPassword; // Return True for valid or false for non valid
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error!: " + ex.Message);
+            }
+            return false; // Return True for valid or false for non valid
+        }
+    }
+}
---
+++ TestEncryption/Program.cs
@@ -0,0 +1,81 @@
+﻿
+using BCrypt;
+
+namespace PasswordHasher
+{
+
+    public class Program
+    {
+        // We create a instance of the Encryption object
+        private static Encryption encrypt = new Encryption();
+
+
+        static bool isActive = true; // Defines if program is running
+        public static void Main(string[] args)
+        {
+            MainMenu();
+        }
+        public static void MainMenu()
+        { // Main Console Menu
+  
+            while (isActive) 
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\n\t Bcrypt Password Hasher by Samuel Gjekic");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n\t1. Create password hash");
+               Console.WriteLine("\t2. Verify password hash");
+               Int32.TryParse(Console.ReadLine(), out int val);
+                switch(val)
+                {
+                    case 1:// Hash a password with Bcrypt hasher
+                        Console.WriteLine("\n\t Enter a password:");
+                        string password = Console.ReadLine();
+                        if (password != "")
+                        {
+                            string hashedPassword = encrypt.EncryptPassword(password);
+                            Console.WriteLine("\n\tPassword Hash: " + hashedPassword);
+                            Console.WriteLine("\n\tPress enter to return to the menu...");
+
+                        }
+                        else if (password == "")
+                        {
+                            Console.WriteLine("\t You entered null!");
+                            Console.WriteLine("\n\tPress enter to return to the menu...");
+                        }
+
+
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        break;
+                    case 2: // Compare a password and a password hash and return true or false
+                        Console.WriteLine("\n\t Enter a password:");
+                        string verifyPassowrd = Console.ReadLine();
+                        Console.WriteLine("\t Enter the password hash:");
+                        string VerifyHash = Console.ReadLine();
+                        bool isValid = encrypt.VerifyPassword(verifyPassowrd, VerifyHash); // Validate the password with the hash and return to bool
+                        if (isValid)
+                        {
+                            Console.WriteLine("\n\t Password and hash matched.");
+                            Console.WriteLine("\t Press enter to return to the menu...");
+                            Console.ReadLine();
+                            Console.Clear();
+                        } else if (!isValid) 
+                        {
+                            Console.WriteLine("\n\t Password and hash did NOT match.");
+                            Console.WriteLine("\t Press enter to return to the menu...");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("\n\tPlease enter a value...");
+                        break;
+                }
+
+            }
+        }
+    }
+}
